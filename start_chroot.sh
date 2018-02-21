7c15a7b7390a1479eecebeb3c4914eeab9759a21
Damián Trigo <dtpineiro@gmail.com>
2018-02-21 07:47:17 +0100

    Making it more generic

--- start_chroot.sh
+++ start_chroot.sh
@@ -6,13 +6,17 @@ mount -t sysfs none $CHR/sys
 mount --bind /dev $CHR/dev
 mount -t tmpfs tmpfs $CHR/dev/shm
 mount -t devpts /dev/pts $CHR/dev/pts
+# mount /lib/modules if you prefer and don't create the folder inside $CHR
 mkdir -p $CHR/lib/modules/2.6.35.9-32-sigma
 mount --bind /lib/modules/2.6.35.9-32-sigma $CHR/lib/modules/2.6.35.9-32-sigma
-mount --bind /home/opt $CHR/home
+# mount something inside home or home as is the largest partition...
+mount --bind /home $CHR/home
 
-# start chrooted ssh and 3 arma2 servers
+# start chrooted ssh and other services
 chroot $CHR service ssh start
+# You need extra modules to load nfsd
 chroot $CHR /sbin/rpcbind
 chroot $CHR /usr/sbin/rpc.nfsd
 chroot $CHR service nfs-kernel-server start
+# Samba performs quite well! And you don't need extra modules
 chroot $CHR service samba start
